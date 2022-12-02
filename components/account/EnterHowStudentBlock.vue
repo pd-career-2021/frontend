27dba1714844a2ca0c17f63ff6f94cc70dcb5fb7
Anastas456 <wild456west@gmail.com>
2022-12-02 17:37:59 +0300

    Partial implementation of component replacement

--- components/account/EnterHowStudentBlock.vue
+++ components/account/EnterHowStudentBlock.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="enter">
     <div class="line">
-<!--      <a href="#" class="back"><img src='~/assets/images/arrow-leftGray.svg'> Назад</a>-->
-      <h1 class="enter__title">Войти как студент</h1>
+        <!-- <a href="#" class="back" v-on:click="back_btn()">
+          <img src='~/assets/images/arrow-leftGray.svg' > Назад
+        </a> -->
+        <div class="title">
+          <h1 class="enter__title">Войти как студент</h1>
+        </div>     
     </div>
-
-
     <form>
       <label for="email">E-mail</label>
       <input id="email" type="email" class="right">
@@ -22,10 +24,19 @@
 
 <script>
 export default {
-  name: 'EnterHowStudentBlock'
+  name: 'EnterHowStudentBlock',
+  data (){
+    return {
+      closed:false
+    }
+  },
+  methods:{
+    back_btn: function(){
+      this.closed = true;
+    },
+    showClosed: function(){
+      console.log(this.closed)
+    }
+  }
 }
 </script>
-
-<style scoped>
-
-</style>
